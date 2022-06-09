0780ba14dcea2cebd663ea9b0bbf2ad5e8ada410
dinesh-58 <gurungsujal@hotmail.com>
2022-06-09 09:19:25 +0545

    Solved day 2, part 2

--- 2015/2_p2.c
+++ 2015/2_p2.c
@@ -4,7 +4,7 @@ void main()
 {
     signed char c;     //signed needed for EOF to work properly
     FILE *fp=fopen("2 input.txt","r");
-    unsigned int n[3]={0,0,0},surface[3]={0,0,0},i,j=0,temp;
+    unsigned int n[3]={0,0,0},perimeter[3]={0,0,0},i,j=0,temp;
     unsigned int total=0;
     while((c=fgetc(fp))!=EOF) //while loop works as long as last line of 
     //"2 input.txt" is blank
@@ -14,34 +14,31 @@ void main()
     {
         if(c=='x'|| c==' ')
         {
-            j++; //move on to storing next dimension
+            j++;
             continue;
         }
         else if(c=='\n')
-        //calculating areas & incrementing total after all 3 dimensions are read 
         {
-            surface[0]=n[0]*n[1], surface[1]=n[1]*n[2], surface[2]=n[2]*n[0];
-            for(i=0;i<2;i++) //sorting areas in ascending order because we 
-            //multiply smallest area by 3 & other areas by 2.
+            perimeter[0]=2*(n[0]+n[1]), perimeter[1]=2*(n[1]+n[2]), perimeter[2]=2*(n[2]+n[0]);
+            for(i=0;i<2;i++) //sorting perimeters in ascending order 
             {
                 for(j=0;j<2;j++)
                 {
-                    if(surface[j]>surface[j+1]){
-                        temp=surface[j];
-                        surface[j]=surface[j+1];
-                        surface[j+1]=temp;
+                    if(perimeter[j]>perimeter[j+1]){
+                        temp=perimeter[j];
+                        perimeter[j]=perimeter[j+1];
+                        perimeter[j+1]=temp;
                     }
                 }
             }
-            total+=(3*surface[0])+(2*surface[1])+(2*surface[2]);
+            total+=perimeter[0]+(n[0]*n[1]*n[2]);
             n[0]=0, n[1]=0, n[2]=0, j=0;
         }
         else
-        //reading dimensions
         {
             n[j]=n[j]*10+((int)c-48);
         }        
     } 
-    printf("The elves need a total of %d sq. feet of wrapping paper",total);
+    printf("The elves need a total of %d feet of ribbon",total);
     fclose(fp);
 }
